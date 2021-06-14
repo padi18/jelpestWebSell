128a1887f27a0c36341c4320ec01be9c5324a01a
Xavi Padilla <xavi.padilla18@gmail.com>
2021-06-14 19:15:49 +0200

    Poniendo fecha en el formulario

--- jelpestSell/websell/forms.py
+++ jelpestSell/websell/forms.py
@@ -1,10 +1,12 @@
-from django.forms import fields
+from django.forms import fields, widgets
 from django.forms.models import ModelForm
 from .models import Demo
 from django import forms
 
 
 class DemoForm(ModelForm):
+    date = forms.DateTimeField(label="Día y hora", input_formats=['%d/%m/%Y %H:%M:%S'], widget=forms.DateTimeInput(attrs={'id': 'datetimepicker'}))
+
     class Meta:
         model = Demo
         fields = '__all__'
@@ -12,5 +14,4 @@ class DemoForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(DemoForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
-            visible.field.widget.attrs['style'] = 'margin-bottom: 2rem'
+            visible.field.widget.attrs['class'] = 'form-control shadow-sm mb-4'
